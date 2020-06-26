382633e3f0372ced3c4beeadc001461af7a3c8b8
Aman Aswal <amanpotato11@gmail.com>
2020-06-26 14:15:24 +0530

    Update anagrams.cpp

--- Strings/anagrams.cpp
+++ Strings/anagrams.cpp
@@ -36,3 +36,46 @@ int main()
 
 	return 0;
 }
+
+// second solution with O(nlogn) time complexity
+// 1. sort 2. compare
+
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+// function to check if two strings are anagram or not
+bool areAnagram(string s1, string s2){
+   int n1 = s1.length();
+   int n2 = s2.length();
+
+   if(n1!=n2){
+      return false;
+   }
+
+   sort(s1.begin(), s1.end());
+   sort(s2.begin(), s2.end());
+
+   for(int i=0; i<n1; i++){
+      if(s1[i]!=s2[i]){
+         return false;
+      }
+   }
+   return true;
+
+}
+
+int main()
+{
+   string a = "test";
+   string b = "sett";
+   if(areAnagram(a,b)){
+      cout<<"anagram";
+   }
+   else{
+      cout<<"not anagram";
+   }
+
+	return 0;
+}
