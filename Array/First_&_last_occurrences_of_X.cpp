aff0177b4be55ee07ae336dd31e62d0c4e2415bb
Aman Aswal <amanpotato11@gmail.com>
2020-07-20 16:33:02 +0530

    Update First_&_last_occurrences_of_X.cpp

--- Array/First_&_last_occurrences_of_X.cpp
+++ Array/First_&_last_occurrences_of_X.cpp
@@ -1,13 +1,16 @@
 #include<iostream>
 using namespace std;
 
-int first_Occurance(int *arr, int n, int x){
+int occurance(int *arr, int n, int x, bool searchFirst){
     int start = 0, end = n-1, result = -1;
     while(start <= end){
         int mid = start + (end-start)/2;
         if(arr[mid] == x){
             result = mid;
-            end = mid - 1; // just change here
+            if(searchFirst)
+                end = mid - 1; // first occurance
+            else 
+                start = mid + 1; // last occurance
         }
         else if(x < arr[mid]){
             end = mid - 1;
@@ -20,24 +23,6 @@ int first_Occurance(int *arr, int n, int x){
     return result;
 }
 
-int last_Occurance(int *arr, int n, int x){
-    int start = 0, end = n-1, result = -1;
-    while(start <= end){
-        int mid = start + (end-start)/2;
-        if(arr[mid] == x){
-            result = mid;
-            start = mid + 1; // just change here
-        }
-        else if(x < arr[mid]){
-            end = mid - 1;
-        }
-        else {
-            start = mid + 1;
-        }
-    }
-    
-    return result;
-}
 
 int main()
  {
@@ -52,12 +37,12 @@ int main()
 	    for(int i=0; i<n; i++)
 	        cin>>arr[i];
 	   
-	    int first = first_Occurance(arr, n, x);
-	    int last = last_Occurance(arr, n, x);
-	    
-	    if(first == -1){
+	    int first = occurance(arr, n, x, true);
+	     if(first == -1){
 	        cout<<-1<<endl;
-	    } else {
+	    } 
+	    else {
+	        int last = occurance(arr, n, x, false);
 	        cout<<first<<" "<<last<<endl;
 	    }
 	}
