5294e69b5f0bff77bab0d61f1c92417b73f9f63f
Aman Aswal <amanpotato11@gmail.com>
2020-08-04 10:18:19 +0530

    Create Sort a stack.cpp

---
+++ Stack/Sort a stack.cpp
@@ -0,0 +1,28 @@
+// 1st approach
+
+void SortedStack :: sort()
+{
+    vector<int> v;
+    while(!s.empty()){
+        v.push_back(s.top());
+        s.pop();
+    }
+    
+    std::sort(v.begin(), v.end());
+    
+    int n=v.size();
+    
+    for(int i=0; i<n; i++){
+        s.push(v[i]);
+    }
+}
+
+//2nd approach
+
+multiset<int> st;
+    while(!s.empty()){
+    st.insert(s.top());
+    s.pop();
+    }
+    for(auto itr=st.begin();itr!=st.end();itr++)
+    s.push(*itr);
